a5b4650fbc3ef3f5091e353794a4a3102292e5be
tdungg81 <dungnguyen8120@gmail.com>
2024-12-13 13:00:23 +0700

    Ex8_Session15

---
+++ Ex8_Session15.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+	
+	char text[]="xin chao sinh vien k24 class jpn_b";
+	int size = strlen(text);
+	for(int i=0; i< size; i++){
+		if(i==0||text[i-1]==' '){
+			if(text[i]>='a'&&text[i]<='z'){
+				text[i]=toupper(text[i]); 
+			} 
+		} 
+	} 
+	printf("%s", text) ;
+	
+	
+	return 0; 
+} 
